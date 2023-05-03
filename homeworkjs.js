a4d44ca2c31d6b7b9d7c69550dbe35a40729960d
SaintAnnA812 <123414583+SaintAnnA812@users.noreply.github.com>
2023-05-03 20:16:55 +0300

    Add files via upload

---
+++ homeworkjs.js
@@ -0,0 +1,40 @@
+let trash = 40000;
+let teacher = 22000;
+console.log(trash + teacher);
+
+let word = 'Владивосток';
+let song = '2000';
+console.log(`${word + " " + song}`);
+
+let n1 = 128;
+let n2 = 254;
+let n3 = 137;
+let n4 = 201;
+console.log(n1 + n2 + n3 + n4);
+
+let a = 2;
+let b = 5;
+let c = a ** b;
+console.log(c);
+
+let message = "Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"
+console.log(`${message}`);
+
+const greetings = "Привет";
+const name = "Кот";
+console.log(`${greetings}, ${name}`);
+
+let time;
+time = 34;
+console.log(time);
+console.log(`Старт поездки. Осталось минут: ` + time);
+time = 15;
+console.log(`Немного сториз в соцсетях. Осталось минут: ` + time);
+time = 10;
+console.log(`Немного не новостей, но событий. Осталось минут: ` + time);
+time = 0;
+console.log(`Вы приехали. Добро пожаловать в Москву`);
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию`);
